6d7bc0972507df90575d2e70738bd78107ff4f9e
Max Philip <mphilip1@student.unimelb.edu.au>
2018-08-12 18:34:20 +1000

    q35 working, but TOO SLOW

--- 21-40/p_euler35.py
+++ 21-40/p_euler35.py
@@ -9,4 +9,33 @@
 
 # How many circular primes are there below one million?
 
+from itertools import permutations
 
+
+def make_rotations(num):
+    return [''.join(p) for p in permutations(num)]
+
+
+def is_prime(x):
+   for i in range(2, (int(x/2)+1)):
+       if (x % i) == 0:
+           return False
+   return True
+
+
+k = int(input("Enter max num: "))
+count = 0
+for num in range(2, k):
+    rotates = make_rotations(str(num))
+
+    all_primes = True
+
+    for perm in rotates:
+        if not is_prime(int(perm)):
+            all_primes = False
+            break
+
+    if all_primes:
+        count += 1
+
+print(count)
